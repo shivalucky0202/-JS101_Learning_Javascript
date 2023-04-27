ba2bbc70fb31c314f5ae3e783f2752e041a93c92
Shiva kumar <shivalucky0202@gmail.com>
2023-04-27 17:43:28 +0530

    variables

---
+++ day_1.js
@@ -0,0 +1,21 @@
+console.log("^^^^^^REPORT CARD^^^^^^");
+let name="shiva";
+console.log("Name-",name);
+
+let school="masai school";
+console.log("School-",school);
+
+let rollno=065;
+console.log("Rollno-",rollno);
+
+let grade="A";
+console.log("Grade-",grade);
+
+let section="english medium";
+console.log("Section-",section);
+
+console.log(`------🅁🄴🅂🅄🄸🅃------`);
+console.log("subject,_____marks");
+console.log("Telugu,______91");
+console.log("English,_____93");
+console.log("Physics,_____95");
